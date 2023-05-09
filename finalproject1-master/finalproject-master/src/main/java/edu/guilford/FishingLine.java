961f286c2d012707d6409fb49cdcbdd81698b693
Kushi-Jakati <jakatikc@guilford.edu>
2023-05-09 14:23:11 -0400

    fixFishingPlacementCommit

--- finalproject1-master/finalproject-master/src/main/java/edu/guilford/FishingLine.java
+++ finalproject1-master/finalproject-master/src/main/java/edu/guilford/FishingLine.java
@@ -20,8 +20,8 @@ public class FishingLine extends GraphicalObjects {
         super();
         xPosition = 339;
         yPosition = 183;
-        endingX = 317;
-        endingY = 827;
+        endingX = 320;
+        endingY = 300;
         line = new Line(xPosition, yPosition, endingX, endingY);
         // set line color
         line.setStrokeWidth(2);
@@ -68,32 +68,6 @@ public class FishingLine extends GraphicalObjects {
                         new KeyValue(line.endYProperty(), 183)));
         lineTransition.setCycleCount(1);
         lineTransition.play();
-
-        // use path transition to have the circle follow the end of the line
-
-        // Circle circle = new Circle(line.getEndX(), line.getEndY(), 5);
-        // circle.setVisible(true);
-        // PathTransition pathTransition = new PathTransition();
-        // path.getElements().add(new MoveTo(339, 183));
-        // path.getElements().add(new MoveTo(317, 827));
-        // pathTransition.setDuration(Duration.seconds(7));
-        // pathTransition.setNode(line);
-        // pathTransition.setPath(path);
-        // pathTransition.setCycleCount(1);
-        // pathTransition.play();
-
-        // //add a circle to the end of the line with parameters(circle center x, circle
-        // center y, radius)
-        // Circle circle = new Circle(line.getEndX(), line.getEndY(), 5);
-        // //create a timeline for the circle
-        // circleTransition = new Timeline();
-        // circleTransition.getKeyFrames()
-        // .add(new KeyFrame(Duration.seconds(7),
-        // new KeyValue(circle.centerXProperty(), 339),
-        // new KeyValue(circle.centerYProperty(), 183)));
-
-        // circleTransition.setCycleCount(1);
-        // circleTransition.play();
     }
 
     // moving line back and forth to catch fish with it being attached to the boat
@@ -117,14 +91,4 @@ public class FishingLine extends GraphicalObjects {
         lineTransition.setCycleCount(1);
         lineTransition.play();
     }
-
-    // Timeline lineTransition2 = new Timeline();
-    // line.setEndX(line.getStartX());
-    // line.setEndY(line.getStartY());
-    // lineTransition2.getKeyFrames()
-    // .add(new KeyFrame(Duration.seconds(10),
-    // new KeyValue(line.endXProperty(), 317),
-    // new KeyValue(line.endYProperty(), 700)));
-    // lineTransition2.setCycleCount(1);
-    // lineTransition2.play();
 }
